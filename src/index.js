67ececc16d8a1daffb37d480ccd3ad807c0fc702
amyciriaco95 <amyciriaco95@gmail.com>
2023-04-01 18:33:36 -0400

    almost commplete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchImg() 
+    fetchBreeds()
+    addBreed()
+})
+
+ function fetchImg(){
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(img => {
+        img.message.forEach(imgElement => {
+            addImg(imgElement)
+        })
+    })
+ }
+
+ function addImg(dogImage){
+    const container = document.getElementById('dog-image-container')
+    const addedElement = document.createElement('img')
+    addedElement.src = (dogImage)
+    container.appendChild(addedElement)
+ }
+
+ function fetchBreeds(){
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then (dogBreed => Object.keys(dogBreed.message).forEach(addBreed))
+ }
+
+ function addBreed(dogBreed){
+    const dogBreeds = document.getElementById('dog-breeds')
+    const li = document.createElement('li')
+    li.textContent = `${dogBreed}`
+    li.addEventListener('click', () => li.style.color = 'red')
+    dogBreeds.appendChild(li)
+ }
